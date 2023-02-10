ff689af9d9048a5b1001ef75e682a3d8bd946928
Ryan Van Valkenburg <ryanvanvalkenburg@gamestop.com>
2023-02-09 16:32:41 -0800

    add hardhat and an NFT sol file for contract address: 0xF6B34BCEda43590Fb8485021fA0Ff213E0eB7014

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
+# Sample Hardhat Project
+
+This project demonstrates a basic Hardhat use case. It comes with a sample contract, a test for that contract, and a script that deploys that contract.
+
+Try running some of the following tasks:
+
+```shell
+npx hardhat help
+npx hardhat test
+REPORT_GAS=true npx hardhat test
+npx hardhat node
+npx hardhat run scripts/deploy.js
+```
+
 ## quick solidity contract deployed on Goerli for setting a mood
  - use `npm -g i lite-server` for html server
+ - use .env for `QUICKNODE_HTTP_URL` and `PRIVATE_KEY`
--- contracts/LW3Token.sol
+++ contracts/LW3Token.sol
@@ -1,10 +1,13 @@
+// FROM REMIX IDE
+
+
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+//pragma solidity ^0.8.9;
 
-import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/ERC20.sol";
+// import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/ERC20.sol";
 
-contract LW3Token is ERC20 {
-    constructor(string memory _name, string memory _symbol) ERC20(_name, _symbol) {
-        _mint(msg.sender, 10 * 10 ** 18);
-    }
-}
+// contract LW3Token is ERC20 {
+//     constructor(string memory _name, string memory _symbol) ERC20(_name, _symbol) {
+//         _mint(msg.sender, 10 * 10 ** 18);
+//     }
+// }
